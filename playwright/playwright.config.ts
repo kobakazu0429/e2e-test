24505f9c3228dba8289e018f9dd4e90501a15196
kobakazu0429 <kobakazu10@gmail.com>
2021-09-01 16:38:53 +0900

    init playwright

---
+++ playwright/playwright.config.ts
@@ -0,0 +1,19 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: "Chromium",
+      use: { browserName: "chromium" },
+    },
+    {
+      name: "Firefox",
+      use: { browserName: "firefox" },
+    },
+    // {
+    //   name: "WebKit",
+    //   use: { browserName: "webkit" },
+    // },
+  ],
+};
+export default config;
