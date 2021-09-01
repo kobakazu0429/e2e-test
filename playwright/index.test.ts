317620600247be3719f2f575b330f0170c9ca1a8
kobakazu0429 <kobakazu10@gmail.com>
2021-09-01 17:12:10 +0900

    remove console.log

--- playwright/index.test.ts
+++ playwright/index.test.ts
@@ -3,6 +3,5 @@ import { test, expect } from "@playwright/test";
 test("basic test", async ({ page }) => {
   await page.goto("https://playwright.dev/");
   const title = page.locator(".navbar__inner .navbar__title");
-  console.log(title);
   await expect(title).toHaveText("Playwright");
 });
